735587b510abf7f42b7450714c5d73e276bbd6b0
Drobertcc <robert.dancso@gmail.com>
2019-10-22 11:01:08 +0200

    % counter added

--- Timer.Designer.cs
+++ Timer.Designer.cs
@@ -29,31 +29,44 @@
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(12, 12);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(559, 46);
+            this.progressBar1.Size = new System.Drawing.Size(569, 46);
             this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
             // 
             // Timer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 108);
+            this.ClientSize = new System.Drawing.Size(592, 74);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Name = "Timer";
             this.Text = "Timer";
             this.Load += new System.EventHandler(this.Timer_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Timer.cs
+++ Timer.cs
@@ -6,6 +6,7 @@ namespace TimerEvents
     public partial class Timer : Form
     {
         System.Windows.Forms.Timer t;
+        int num = 1;
         public Timer()
         {
             InitializeComponent();
@@ -16,12 +17,14 @@ namespace TimerEvents
             t = new System.Windows.Forms.Timer();
             t.Interval = 100;
             t.Tick += new EventHandler(t_Tick);
+            label1.Text = "" + num;
             t.Start();
         }
 
         void t_Tick(object sender, EventArgs e)
         {
             progressBar1.Value += 1;
+            label1.Text = "" + num++;
             if (progressBar1.Value >= 100) t.Stop();
         }
     }
