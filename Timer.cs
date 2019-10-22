ecf18dcb77ef0b030ee9e3477069b822b49f995f
Drobertcc <robert.dancso@gmail.com>
2019-10-22 10:30:55 +0200

    Add project files.

---
+++ Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Timer : Form
+    {
+        System.Windows.Forms.Timer t;
+        public Timer()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Load(object sender, EventArgs e)
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 100;
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 1;
+            if (progressBar1.Value >= 100) t.Stop();
+        }
+    }
+}
